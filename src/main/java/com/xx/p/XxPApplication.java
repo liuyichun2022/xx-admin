cc5391563d9a73f8014f4b4b6961d272c34290d5
liuyichun <liuyichun@chinapji.com>
2023-03-24 16:42:32 +0800

    init

---
+++ src/main/java/com/xx/p/XxPApplication.java
@@ -0,0 +1,13 @@
+package com.xx.p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxPApplication.class, args);
+    }
+
+}
